20a84e49f1de0c9dd3a4a789dc1cd820fc9014b1
geekact <geekacting@gmail.com>
2024-09-17 17:36:37 +0800

    增加packageManager

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import path from 'node:path/posix';
 import yargsParser from 'yargs-parser';
 import { existsSync } from 'node:fs';
 import { cp, mkdir, readdir, rm, stat } from 'node:fs/promises';
-import { Listr } from 'listr2';
+import { terminal } from '@aomex/console';
 import { styleText } from 'node:util';
 import { execSync, spawn } from 'node:child_process';
 import { replaceVariables } from './replace-variables';
@@ -48,129 +48,126 @@ const runShell = async (command: string) => {
 };
 const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
-const spinner = new Listr([]);
-
-spinner.add({
-  title: '创建目录',
-  task: async () => {
-    if (existsSync(targetDir)) {
-      await rm(targetDir, { recursive: true, force: true });
-    }
-    await mkdir(targetDir, { recursive: true });
-    process.chdir(path.resolve(projectName));
+const { error } = await terminal.runTasks([
+  {
+    title: '创建目录',
+    task: async () => {
+      if (existsSync(targetDir)) {
+        await rm(targetDir, { recursive: true, force: true });
+      }
+      await mkdir(targetDir, { recursive: true });
+      process.chdir(path.resolve(projectName));
+    },
   },
-});
-
-spinner.add({
-  title: '复制模板文件',
-  task: async () => {
-    const variables = {
-      projectName,
-      packageManager,
-      nodeVersion,
-      packageManagerVersion,
-    };
-    await cp(templateDir, targetDir, { recursive: true });
-    const files = await readdir(targetDir, { recursive: true });
-    for (const file of files) {
-      const isFile = (await stat(path.join(targetDir, file))).isFile();
-      if (!isFile) continue;
-      await replaceVariables(file, variables);
-    }
-    await sleep();
+  {
+    title: '复制模板文件',
+    task: async () => {
+      const variables = {
+        projectName,
+        packageManager,
+        nodeVersion,
+        packageManagerVersion,
+      };
+      await cp(templateDir, targetDir, { recursive: true });
+      const files = await readdir(targetDir, { recursive: true });
+      for (const file of files) {
+        const isFile = (await stat(path.join(targetDir, file))).isFile();
+        if (!isFile) continue;
+        await replaceVariables(file, variables);
+      }
+      await sleep();
+    },
   },
-});
-
-spinner.add({
-  title: 'git初始化',
-  task: async () => {
-    await runShell('git init');
-    await sleep();
+  {
+    title: 'git初始化',
+    task: async () => {
+      await runShell('git init');
+      await sleep();
+    },
   },
-});
-
-spinner.add({
-  title: '增加volta配置',
-  skip: async () => {
-    return !/\d\.\d/.test(execSync('volta -v', { encoding: 'utf8' }));
+  {
+    title: '增加volta配置',
+    skip: async () => {
+      return !/\d\.\d/.test(execSync('volta -v', { encoding: 'utf8' }));
+    },
+    task: async () => {
+      await runShell(`volta pin node@${nodeVersion}`);
+      if (packageManager !== 'npm') {
+        await runShell(`volta pin ${packageManager}@${packageManagerVersion}`);
+      }
+    },
   },
-  task: async () => {
-    await runShell(`volta pin node@${nodeVersion}`);
-    if (packageManager !== 'npm') {
-      await runShell(`volta pin ${packageManager}@${packageManagerVersion}`);
-    }
+  {
+    title: '安装插件',
+    task: async (_, task) => {
+      const packages: { label: string; pkgs: string[]; dev?: boolean }[] = [
+        {
+          label: 'dependencies',
+          pkgs: [
+            '@aomex/core',
+            '@aomex/web',
+            '@aomex/cors',
+            '@aomex/etag',
+            '@aomex/compress',
+            '@aomex/http-logger',
+            '@aomex/response-time',
+            '@aomex/console',
+            '@aomex/cron',
+            '@aomex/helmet',
+            '@aomex/openapi',
+            '@aomex/swagger-ui',
+            '@aomex/async-trace',
+            '@prisma/client',
+            '@aomex/cache',
+            '@aomex/cache-redis-adapter',
+            'ioredis',
+          ],
+        },
+        {
+          label: 'dev dependencies',
+          pkgs: [
+            'prisma',
+            'typescript',
+            'tsx',
+            'tsc-alias',
+            '@types/node',
+            'husky',
+            'prettier',
+            '@commitlint/cli',
+            '@commitlint/config-conventional',
+            'eslint',
+            '@typescript-eslint/eslint-plugin',
+            '@typescript-eslint/parser',
+            'eslint-plugin-check-file',
+          ],
+          dev: true,
+        },
+      ];
+      const action = packageManager === 'npm' ? 'install' : 'add';
+      const devSuffix = packageManager === 'npm' ? '--save-dev' : '-D';
+
+      for (let i = 0; i < packages.length; ++i) {
+        const { pkgs, dev, label } = packages[i]!;
+        task.title = '安装插件 ' + styleText('gray', label);
+        await runShell(
+          `${packageManager} ${action} ${pkgs.join(' ')} ${
+            dev ? devSuffix : ''
+          }`,
+        );
+      }
+      task.title = '安装插件';
+    },
   },
-});
-
-spinner.add({
-  title: '安装插件',
-  task: async (_, task) => {
-    const packages: { label: string; pkgs: string[]; dev?: boolean }[] = [
-      {
-        label: 'dependencies',
-        pkgs: [
-          '@aomex/core',
-          '@aomex/web',
-          '@aomex/cors',
-          '@aomex/etag',
-          '@aomex/compress',
-          '@aomex/http-logger',
-          '@aomex/response-time',
-          '@aomex/console',
-          '@aomex/cron',
-          '@aomex/helmet',
-          '@aomex/openapi',
-          '@aomex/swagger-ui',
-          '@aomex/async-trace',
-          '@prisma/client',
-          '@aomex/cache',
-          '@aomex/cache-redis-adapter',
-          'ioredis',
-        ],
-      },
-      {
-        label: 'dev dependencies',
-        pkgs: [
-          'prisma',
-          'typescript',
-          'tsx',
-          'tsc-alias',
-          '@types/node',
-          'husky',
-          'prettier',
-          '@commitlint/cli',
-          '@commitlint/config-conventional',
-          'eslint',
-          '@typescript-eslint/eslint-plugin',
-          '@typescript-eslint/parser',
-          'eslint-plugin-check-file',
-        ],
-        dev: true,
-      },
-    ];
-    const action = packageManager === 'npm' ? 'install' : 'add';
-    const devSuffix = packageManager === 'npm' ? '--save-dev' : '-D';
-
-    for (let i = 0; i < packages.length; ++i) {
-      const { pkgs, dev, label } = packages[i]!;
-      task.title = '安装插件 ' + styleText('gray', label);
-      await runShell(
-        `${packageManager} ${action} ${pkgs.join(' ')} ${dev ? devSuffix : ''}`,
-      );
-    }
-    task.title = '安装插件';
+  {
+    title: '生成prisma客户端',
+    task: async () => {
+      await runShell('npx prisma generate');
+      await sleep();
+    },
   },
-});
+]);
 
-spinner.add({
-  title: '生成prisma客户端',
-  task: async () => {
-    await runShell('npx prisma generate');
-    await sleep();
-  },
-});
-
-await spinner.run();
+if (error) process.exit(1);
 
 console.log(
   '\n项目创建成功：' +
