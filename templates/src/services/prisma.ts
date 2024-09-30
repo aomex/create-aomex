09e69701ff60dd61451c912075aea9e7e5092dc6
geekact <geekacting@gmail.com>
2024-09-30 20:02:17 +0800

    增加prisma事物实例类型

--- templates/src/services/prisma.ts
+++ templates/src/services/prisma.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import * as runtime from '@prisma/client/runtime/library';
 
 export const prisma = new PrismaClient({
   log: ['info'],
 });
+
+export type PrismaTX = Omit<PrismaClient, runtime.ITXClientDenyList>;
