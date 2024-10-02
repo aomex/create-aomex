807f241feab084fe3dea6c3e176a520ddc2c8714
geekact <geekacting@gmail.com>
2024-10-02 15:50:35 +0800

    合并deploy脚本

--- templates/scripts/deploy.sh
+++ templates/scripts/deploy.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
-sudo docker build --tag {{projectName}}:production --file Dockerfile.production .
+env="$1"
 
-cron_container_name={{projectName}}-cron-production
+sudo docker build --tag "{{projectName}}:$env" --file "Dockerfile.$env" .
+
+cron_container_name="{{projectName}}-cron-$env"
 container_exist=$(sudo docker ps | { grep $cron_container_name || :; })
 if [ -n "$container_exist" ];
 then
   sudo docker exec it $cron_container_name /bin/sh -c "npx aomex cron:stop"
 fi
 
-sudo docker compose --file docker-compose-production.yml up -d --timeout=1
+sudo docker compose --file "docker-compose-$env.yml" up -d --timeout=1
