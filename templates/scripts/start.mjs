d9ba7a138022e15ccbd372716eae928e1c44a6fc
geekact <geekacting@gmail.com>
2025-03-09 23:35:35 +0800

    升级启动脚本

--- templates/scripts/start.mjs
+++ templates/scripts/start.mjs
@@ -2,34 +2,17 @@ import { execSync, spawn } from 'node:child_process';
 import process from 'node:process';
 
 execSync('docker compose -f docker-compose.yml down', { stdio: 'inherit' });
+execSync('docker compose -f docker-compose.yml up --wait', { stdio: 'inherit' });
+const ps = execSync('docker compose -f docker-compose.yml ps', { stdio: 'pipe', encoding: 'utf8' });
+if (ps.includes('unhealthy')) process.exit(1);
 
-const docker = spawn('docker compose -f docker-compose.yml up --wait', {
-  stdio: ['inherit', 'inherit', 'pipe'],
-  shell: true,
+execSync('npx prisma generate', { stdio: 'inherit' });
+execSync('npx prisma migrate deploy', { stdio: 'inherit' });
+const api = spawn('node', ['--import', 'tsx/esm', '--watch', 'src/web.ts'], { stdio: 'inherit' });
+const cron = spawn('npx', ['aomex', 'cron:start'], { stdio: 'inherit' });
+
+process.on('SIGINT', () => {
+  cron.kill('SIGINT');
+  api.kill('SIGINT');
+  execSync('docker compose -f docker-compose.yml down', { stdio: 'inherit' });
 });
-
-await new Promise((resolve) => {
-  const reg = /mysql.+?healthy/i;
-  docker.stderr.on('data', (chunk) => {
-    process.stderr.write(chunk);
-    if (reg.test(chunk.toString())) {
-      resolve();
-    }
-  });
-});
-
-const server = spawn(
-  'npx prisma generate && npx prisma migrate deploy && node --import tsx/esm --watch src/web.ts',
-  { stdio: 'inherit', shell: true },
-);
-
-const cron = spawn('npx aomex cron:start', { stdio: 'inherit', shell: true });
-
-// 防止被其它逻辑清除监听
-setTimeout(() => {
-  process.on('SIGINT', () => {
-    cron.kill('SIGINT');
-    server.kill('SIGINT');
-    docker.kill('SIGINT');
-  });
-}, 1000);
