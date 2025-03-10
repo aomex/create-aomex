d17c2d26a01045334ac711d82643365ef3527c3c
geekact <geekacting@gmail.com>
2025-03-10 20:27:27 +0800

    更新模板

--- templates/src/middleware/http-logger.md.ts
+++ templates/src/middleware/http-logger.md.ts
@@ -1,9 +1,9 @@
-import { Logger } from '@aomex/common';
+import { Logger } from '@aomex/logger';
 import { httpLogger as aomexHttpLogger } from '@aomex/http-logger';
 
 export const httpLogger = aomexHttpLogger({
   transports: [
-    new Logger.transport.Console(),
-    new Logger.transport.File({ file: 'logs/http/%year%-%month%-%day%/%hour%.log' }),
+    new Logger.transports.Console(),
+    new Logger.transports.File({ file: 'logs/http/{year}-{month}-{day}/{hour}.log' }),
   ],
 });
