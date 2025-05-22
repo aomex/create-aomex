211b77aeff2595e88578410fe9ceee07ecab2133
geekact <geekacting@gmail.com>
2025-05-22 09:28:23 +0800

    更新依赖项至版本3.27.1，并修改慢追踪中记录的属性名称

--- templates/src/middleware/slow-trace.md.ts
+++ templates/src/middleware/slow-trace.md.ts
@@ -1,8 +1,8 @@
 import { traceMiddleware } from '@aomex/async-trace';
 import type { WebContext } from '@aomex/web';
 
-export const slowTrace = traceMiddleware('生命周期', async (record, _ctx: WebContext) => {
-  if (record.delta > 2_000) {
+export const slowTrace = traceMiddleware('链路追踪', async (record, _ctx: WebContext) => {
+  if (record.duration > 2_000) {
     // 上报慢日志
   }
 });
