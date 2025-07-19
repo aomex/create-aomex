6a7b5d544a348114c33f4f55d22233344325d33c
geekact <geekacting@gmail.com>
2025-07-19 21:01:47 +0800

    新的auth用法

--- templates/src/middleware/auth.md.ts
+++ templates/src/middleware/auth.md.ts
@@ -1,6 +1,6 @@
-import { Authentication } from '@aomex/auth';
+import { Auth } from '@aomex/auth';
 
 // 身份认证，参考文档：https://aomex.js.org/plugins/auth.html
-export const auth = new Authentication({
+export const auth = new Auth({
   strategies: {},
 });
