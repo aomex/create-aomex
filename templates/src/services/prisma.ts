993860ed61c0b47b8c4738cc703da992aff9ffb3
geekact <geekacting@gmail.com>
2025-12-04 19:33:58 +0800

    更新模板

--- templates/src/services/prisma.ts
+++ templates/src/services/prisma.ts
@@ -1,16 +1,8 @@
 import { PrismaClient } from '../generated/prisma/client';
 import * as runtime from '@prisma/client/runtime/client';
-
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-const config = new URL(process.env['DATABASE_URL']!);
-const adapter = new PrismaMariaDb({
-  host: config.hostname,
-  port: parseInt(config.port),
-  user: config.username,
-  password: config.password,
-  database: config.pathname.slice(1),
-});
+const adapter = new PrismaMariaDb(process.env['DATABASE_URL']!);
 
 export const prisma = new PrismaClient({
   adapter,
